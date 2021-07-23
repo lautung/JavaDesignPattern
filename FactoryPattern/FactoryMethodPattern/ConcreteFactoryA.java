d80086a974b0ecd0e6ed147c9880ffac4c64d2e9
lautung <2281876422@qq.com>
2021-07-23 20:16:39 +0800

    medified

--- FactoryPattern/FactoryMethodPattern/ConcreteFactoryA.java
+++ FactoryPattern/FactoryMethodPattern/ConcreteFactoryA.java
@@ -3,7 +3,7 @@ package FactoryPattern.FactoryMethodPattern;
 public class ConcreteFactoryA implements AbstractFactory{
 
     @Override
-    public AbstractProduct newProduct() {
+    public AbstractProduct newProductA() {
         System.out.println("具体工厂A生成-->具体产品A...");
         return new ConcreteProductA();
     }
--- FactoryPattern/FactoryMethodPattern/ConcreteFactoryB.java
+++ FactoryPattern/FactoryMethodPattern/ConcreteFactoryB.java
@@ -3,7 +3,7 @@ package FactoryPattern.FactoryMethodPattern;
 public class ConcreteFactoryB implements AbstractFactory {
 
     @Override
-    public AbstractProduct newProduct() {
+    public AbstractProduct newProductA() {
         System.out.println("具体工厂B生成-->具体产品B...");
         return new ConcreteProductB();
     }
